865991aed62eff730a8212e5058737ceae360ac7
oscar <oscarchan96@outlook.com>
2022-09-13 00:53:06 -0500

    feat: agregado el primer boton en la imagen

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ for (let i = 1; i <= 30; i++) {
     card_image.classList.add("card-image");
     card_image.innerHTML = `<img src="/img/pict (${i}).jpg" alt="imagen">
                             <div class="overview">
-                            <button type="button" class="know-more" data-bs-toggle="modal" data-bs-target="#exampleModal">View</button>
+                            <button type="button" class="know-more">View</button>
                             </div>`;
     grid_content.appendChild(card_image);
 }
