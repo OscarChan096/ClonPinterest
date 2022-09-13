3f203063ce43dce49759a667b8e32ae5de55e616
oscar <oscarchan96@outlook.com>
2022-09-13 00:32:58 -0500

    fix: corregido el problema del tdesbordamiento de imagenes de la pagina en horizontal

--- js/index.js
+++ js/index.js
@@ -1,12 +1,11 @@
 const grid_content = document.getElementById("grid-content");
-const row = document.createElement("div");
-row.className = "row-card-image";
-grid_content.appendChild(row);
 
 for (let i = 1; i <= 30; i++) {
     const card_image = document.createElement("div");
     card_image.classList.add("card-image");
-    card_image.innerHTML = `<img src="/img/pict (${i}).jpg" alt="imagen">`;
-    row.appendChild(card_image);
+    card_image.innerHTML = `<img src="/img/pict (${i}).jpg" alt="imagen">
+                            <div class="overview">
+                            <button type="button" class="know-more" data-bs-toggle="modal" data-bs-target="#exampleModal">View</button>
+                            </div>`;
+    grid_content.appendChild(card_image);
 }
-
