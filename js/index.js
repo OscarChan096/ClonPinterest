1595c4c67355e96ec3216f0b2fc0b9b6c72a31d8
oscar <oscarchan96@outlook.com>
2022-09-14 18:59:43 -0500

    feat: agregado los 3 botones a las imagenes

--- js/index.js
+++ js/index.js
@@ -5,7 +5,20 @@ for (let i = 1; i <= 30; i++) {
     card_image.classList.add("card-image");
     card_image.innerHTML = `<img src="/img/pict (${i}).jpg" alt="imagen">
                             <div class="overview">
-                            <button type="button" class="know-more">View</button>
+                            <div class="header-card-image">
+                            <button type="button" class="save-btn">Guardar</button>
+                            </div>
+                            <div class="footer-card-image">
+                            <div class="url-btn">
+                            <input type="button" class="url" value="/img/pict (${i}).jpg">
+                            </div>
+                            <div class="up-icon-div">
+                            <input type="image" src="./img/up.png" class="up">
+                            </div>
+                            <div class="more-btn">
+                            <input type="image" src="./img/puntos.png" class="more">
+                            </div>
+                            </div>
                             </div>`;
     grid_content.appendChild(card_image);
 }
