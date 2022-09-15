b5138c014edb3367e5dd7ff1eb6f3a4b70e5e8a7
oscar <oscarchan96@outlook.com>
2022-09-14 23:50:21 -0500

    fix: errores menores de disÃeño

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const grid_content = document.getElementById("grid-content");
 for (let i = 1; i <= 30; i++) {
     const card_image = document.createElement("div");
     card_image.classList.add("card-image");
-    card_image.innerHTML = `<img src="/img/pict (${i}).jpg" alt="imagen">
+    card_image.innerHTML = `<img src="./img/pict (${i}).jpg" alt="imagen">
                             <div class="overview">
                             <div class="header-card-image">
                             <button type="button" class="save-btn">Guardar</button>
